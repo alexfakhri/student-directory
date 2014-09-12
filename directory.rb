711b933b748266759b67f4206748241fb8a0d4c4
Alex Fakhri <alexfakhri@me.com>
2014-09-12 11:48:51 +0100

    Added variable and interpolation to code

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 9
+
 #first we print the list of students
 puts "The students of my cohort at Makers Academy"
 puts "----------"
@@ -11,8 +13,5 @@ puts "The Terminator"
 puts "Freddy Kruger"
 puts "The Joker"
 #finally, we print the total
-print "Overall, we have "
-#it's important that print () doesn't add new line of characters
-print 9
-puts " great students"
+puts "Overall, we have #{student_count} great students"
 
